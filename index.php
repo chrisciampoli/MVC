74b0290b3a6b879cb79a2fe1b23a33f731751c52
Chris Ciampoli <chrisciampoli@gmail.com>
2013-01-23 23:17:00 -0800

    changes

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Define document paths
+ */
+define('SERVER_ROOT' , '/Applications/MAMP/htdocs/MVC');
+define('SITE_ROOT' , 'http://localhost');
+
+/**
+ * Fetch the router
+ */
+require_once(SERVER_ROOT . '/controllers/' . 'router.php');
